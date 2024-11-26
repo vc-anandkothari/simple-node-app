1bb7cbe0b5713461e810a36019614a0b9bb860f3
vc-anandkothari <109572628+vc-anandkothari@users.noreply.github.com>
2024-11-27 05:21:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3002;
+const port = 3000;
 
 // Built-in Middleware to parse JSON data in the request body
 app.use(bodyParser.json());
 
 // Use the routes in your app
 app.get('/', (req, res) => {
-  res.send("Hello! from Node JS Application");
+  res.send(`Hello! from Node JS Application -  ${PORT}`);
 });
+
 app.get('/cars', (req, res) => {
   res.send("Response from Cars API");
 });
